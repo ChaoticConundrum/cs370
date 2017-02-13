103903a8cadfe0cf8e14559925f675aaa51a0253
Charlie Waters <cawiii@me.com>
2017-02-13 10:22:51 -0500

    Debugging extnernal program execution

--- assignment1/main.c
+++ assignment1/main.c
@@ -29,6 +29,10 @@ int search(char *name){
     // Search through PATH
     char *tok = strtok(path, PATH_TOK);
     while(tok != NULL){
+        if(strlen(tok) + strlen(name) + 2 > PATH_LEN_MAX){
+            fprintf(stderr, "path is too long\n");
+            return -2;
+        }
         // Prepare path
         strcpy(program, tok);
         strcat(program, "/");
@@ -44,16 +48,17 @@ int search(char *name){
         tok = strtok(NULL, PATH_TOK);
     }
     // Did not find program
-    return -2;
+    return 1;
 }
 
-int external(const char **args){
+int external(const char *path, char **args){
+    printf("path: %s\n", path);
 
     pid_t pid = fork();
 
     if(pid == 0){
         // Child
-        int err = execvp(args[0], args);
+        int err = execvp(path, args);
         // we shouldn't be here
         fprintf(stderr, "execl error %d %d %s\n", err, errno, strerror(errno));
         exit(EXIT_FAILURE);
@@ -75,102 +80,107 @@ int external(const char **args){
     return 0;
 }
 
-int process(const char *cmd){
+int process(const char *input, int *status){
 
-    char buffer[CMD_LEN_MAX];
-    strcpy(buffer, cmd);
+    char argbuff[CMD_LEN_MAX];
+    strcpy(argbuff, input);
 
     int argn = 0;
     char *args[ARG_MAX];
 
     // Split arguments
-    unsigned int len = strlen(buffer);
-    char *base = buffer;
+    unsigned int len = strlen(argbuff);
+    char *base = argbuff;
     int quote = 0;
 
     for(int i = 0; i < len; ++i){
-        if(buffer[i] == "\n"){
-            buffer[i] = 0;          // Insert terminator
+        if(argbuff[i] == '\n'){
+            argbuff[i] = 0;          // Insert terminator
             break;
-        } else if(buffer[i] == '"'){
+        } else if(argbuff[i] == '"'){
             // Hit double quote
             if(quote == 0){
                 // Enter quote mode
                 quote = 1;
-                base = buffer + i + 1;  // Set next arg base
+                base = argbuff + i + 1;  // Set next arg base
             } else {
-                buffer[i] = 0;          // Insert terminator
-                args[argn++] = base;    // Push arg
-                base = buffer + i + 1;  // Set next arg base
+                argbuff[i] = 0;          // Insert terminator
+                args[argn++] = base;     // Push arg
+                base = argbuff + i + 1;  // Set next arg base
                 // Exit quote mode
                 quote = 0;
             }
-        } else if(quote == 0 && buffer[i] == ' '){
+        } else if(quote == 0 && argbuff[i] == ' '){
             // Hit space
-            buffer[i] = 0;          // Insert terminator
-            if(base != buffer + i)
-                args[argn++] = base;    // Push arg
-            base = buffer + i + 1;  // Set next arg base
+            argbuff[i] = 0;          // Insert terminator
+            if(base != argbuff + i)
+                args[argn++] = base; // Push arg
+            base = argbuff + i + 1;  // Set next arg base
         }
     }
-    if(base != buffer + len - 1)
+    if(base != argbuff + len - 1)
         args[argn++] = base;    // Push last arg
 
-    printf("argn: %d\n", argn);
-    for(int i = 0; i < argn; ++i){
-        printf("%d: %s\n", i, args[i]);
-    }
+    args[argn] = 0; // terminate arg list
+    *status = 0;
+    const char *cmd = args[0];
 
-    return 0;
-
-    /*
-    char *tok = strtok(cmd, ARG_TOK);
-    while(tok != NULL){
-
-        // Prepare path
-        strcpy(program, tok);
-        strcat(program, "/");
-        strcat(program, name);
-        // Check path
-        int ret = access(program, X_OK);
-        if(ret == 0){
-            // Found program
-            strcpy(name, program);
-            return 0;
-        }
-        // Try again
-        tok = strtok(NULL, ARG_TOK);
-    }
-
-    for(int i = 0; i < CMD_LEN_MAX; ++i){
-        if(cmd[i] == ' '){
-            if(strlen(buffer) > 0)
-                continue;
-        }
-    }
+//    printf("input '%s'\n", input);
+//    printf("argn %d\n", argn);
+//    for(int i = 0; i < argn; ++i)
+//        printf("arg %d: '%s'\n", i, args[i]);
 
     // Handle command
-    if(strcmp(cmd, "ping") == 0){
-        printf("Pong!\n");
-        return 0;
-    } else {
-        char path[PATH_LEN_MAX];
-        strcpy(path, args[0]);
-        // Search for program
-        if(search(path) == 0){
-            // Call external program
-            external(args);
-        }
-    }
-    */
+    if(strcmp(cmd, "exit") == 0){
+        // Exit shell
+        *status = 1;
 
-    printf("Unknown Command\n");
-    return -1;
+    } else if(strcmp(cmd, "echo") == 0){
+        // Echo arguments
+        char buffer[CMD_LEN_MAX];
+        buffer[0] = 0;
+        for(int i = 1; i < argn; ++i){
+            strcat(buffer, args[i]);
+            strcat(buffer, " ");
+        }
+        printf("%s\n", buffer);
+
+    } else if(strcmp(cmd, "cd") == 0){
+        if(argn > 1){
+            // Change directory
+            chdir(args[1]);
+        }
+
+    } else if(strcmp(cmd, "pwd") == 0){
+        // Print working directory
+        char buffer[PATH_LEN_MAX];
+        getcwd(buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+
+    } else {
+        // External program
+        char path[PATH_LEN_MAX];
+        strcpy(path, cmd);
+        // Search for program
+        int ret = search(path);
+        if(ret == 0){
+            *status = 3;
+            // Call external program
+            return external(path, args);
+        } else if(ret < 0){
+            // error
+            *status = 4;
+        }
+        // Program not found
+        *status = 2;
+    }
+    return 0;
 }
 
 int main(int argc, const char **argv){
 
     char cmd[CMD_LEN_MAX];
+    int status = 0;
 
     while(1){
         // First check stdin
@@ -181,7 +191,7 @@ int main(int argc, const char **argv){
         }
 
         // Write prompt
-        printf("> ");
+        printf("choong-sh> ");
 
         // Read line
         if(fgets(cmd, CMD_LEN_MAX, stdin) == NULL){
@@ -192,8 +202,21 @@ int main(int argc, const char **argv){
         }
 
         // Process input
-        int ret = process(cmd);
-        printf("return: %d\n", ret);
+        int ret = process(cmd, &status);
+//        printf("status %d\n", status);
+
+        if(status == 0){
+            // internal command
+        } else if(status == 1){
+            // exit
+            break;
+        } else if(status == 2){
+            // unknown command
+            printf("Unknown Command\n");
+        } else if(status == 3){
+            // external command
+            printf("return: %d\n", ret);
+        }
     }
 
     return 0;
