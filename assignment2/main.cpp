878ddc0458ec76a5a570f00ba3ff6bfb7627297e
Charlie Waters <cawiii@me.com>
2017-03-17 01:42:03 -0400

    some producers not running

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -11,6 +11,7 @@ using namespace LibChaos;
 #include <sys/wait.h>
 #include <errno.h>
 #include <sys/mman.h>
+#include <iostream>
 
 struct Job {
     int id;
@@ -28,38 +29,53 @@ struct Share {
 
 void runProducer(zu64 i, Share *share){
     int atime = share->arate;
-    LOG("Producer " << i << " start");
-    while(true){
+    printf("producer %d\n", i);
+//    LOG("Producer " << i << " start");
+    bool run = true;
+    while(run){
         share->lock->lock();
-        if(share->total == 0){
-            share->lock->unlock();
-            break;
-        }
 
-        Job j = { share->total, share->total == 1 };
-        LOG("Queue " << j.id);
-        share->queue->addWork(j);
-        share->total--;
+        if(share->total)
+            share->total--;
+        else
+            run = false;
+
+        int id = share->total;
+
         share->lock->unlock();
 
-        if(share->total == 0)
-            break;
-        ZThread::msleep(atime);
+        if(run){
+            Job j = { id, !id };
+//            LOG("Queue " << j.id);
+            printf("%d queue %d\n", i, j.id);
+            share->queue->addWork(j);
+
+            ZThread::msleep(atime);
+        }
     }
-    LOG("Producer " << i << " done");
+//    LOG("Producer " << i << " done");
 }
 
 void runConsumer(zu64 i, Share *share){
     int stime = share->srate;
-    LOG("Consumer " << i << " start");
-    while(true){
+    printf("consumer %d\n", i);
+//    LOG("Consumer " << i << " start");
+    bool run = true;
+    while(run){
         Job j = share->queue->getWork();
-        LOG("Job " << j.id);
-        ZThread::msleep(stime);
-        if(j.exit)
-            break;
+//        LOG("Job " << j.id);
+        printf("%d job %d\n", i, j.id);
+
+        if(j.exit){
+            printf("stop\n");
+            share->queue->addWork(j);
+            run = false;
+        }
+
+        if(run)
+            ZThread::msleep(stime);
     }
-    LOG("Consumer " << i << " done");
+//    LOG("Consumer " << i << " done");
 }
 
 int main(int argc, char **argv){
@@ -80,23 +96,21 @@ int main(int argc, char **argv){
         return -1;
     }
 
-    ZMutex *lock = new ZMutex(ZMutex::PSHARE);
-
     // pretty neat
     ZAllocator<zbyte> *alloc = new ZPoolAllocator<zbyte>(pool, psize);
 
+
     // oh god i'm sorry
+    ZAllocator<Share> *salloc = new ZWrapAllocator<Share>(alloc);
+    ZAllocator<ZMutex> *lalloc = new ZWrapAllocator<ZMutex>(alloc);
     ZAllocator<ZWorkQueue<Job>> *qalloc = new ZWrapAllocator<ZWorkQueue<Job>>(alloc);
     ZAllocator<typename ZList<Job>::Node>*jalloc = new ZWrapAllocator<typename ZList<Job>::Node>(alloc);
-    ZAllocator<Share> *salloc = new ZWrapAllocator<Share>(alloc);
 
     // actual black magic
-    ZWorkQueue<Job> *queue = qalloc->construct(qalloc->alloc(), 1, jalloc, ZCondition::PSHARE);
-
-
     Share *share = salloc->construct(salloc->alloc(), 1);
-    share->lock = lock;
-    share->queue = queue;
+    share->lock = lalloc->construct(lalloc->alloc(), 1, ZMutex::PSHARE);
+    share->queue = qalloc->construct(qalloc->alloc(), 1, jalloc, ZCondition::PSHARE);
+
     share->total = requests;
     share->arate = 1000 / arate;
     share->srate = 1000 / srate;
@@ -106,9 +120,9 @@ int main(int argc, char **argv){
 
     // start producers
     for(int i = 0; i < nproducer; ++i){
+        LOG("Fork producer " << i);
         pid_t pid = fork();
         if(pid == 0){
-            LOG("Start producer " << i);
             runProducer(i, share);
             return 0;
         } else if(pid == -1){
@@ -117,9 +131,9 @@ int main(int argc, char **argv){
     }
     // start consumers
     for(int i = 0; i < nconsumer; ++i){
+        LOG("Fork consumer " << i);
         pid_t pid = fork();
         if(pid == 0){
-            LOG("Start consumer  " << i);
             runConsumer(i, share);
             return 0;
         } else if(pid == -1){
@@ -136,16 +150,19 @@ int main(int argc, char **argv){
 
     LOG("Done Waiting");
 
+    qalloc->destroy(share->queue);
+    qalloc->dealloc(share->queue);
+
+    lalloc->destroy(share->lock);
+    lalloc->dealloc(share->lock);
+
     salloc->destroy(share);
     salloc->dealloc(share);
 
-    delete lock;
-
-    qalloc->destroy(queue);
-    qalloc->dealloc(queue);
-
+    delete salloc;
     delete jalloc;
     delete qalloc;
+    delete lalloc;
 
     return 0;
 }
